d7a1adcedcb1bbd89c35f6b3ecb1969cefe540d5
Alexandre <alexandre_viegas@icloud.com>
2023-11-19 18:54:13 +0100

    ⬆️ Upgrade react router dom

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import EditProfile from "./pages/EditProfile";
@@ -8,17 +8,31 @@ import Feed from "./pages/Feed";
 import Error from "./components/Error";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/feed",
+    element: <Feed />,
+  },
+  {
+    path: "/edit_profile/:id",
+    element: <EditProfile />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/edit_profile/:id" element={<EditProfile />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
